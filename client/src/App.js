8fd8175129679c57b23ac1bcdf4192450a168f5e
Chris Pfaff <chrispfaff10@gmail.com>
2019-09-26 17:36:32 -0500

    ready to imploy file buffer from backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 import * as d3 from "d3";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>L7Informatics</header>
+    </div>
+  );
 }
 
 export default App;
