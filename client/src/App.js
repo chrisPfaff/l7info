98dd4415fe78daec3c6e54aee66a30abaf8e048f
Chris Pfaff <chrispfaff10@gmail.com>
2019-09-26 17:19:53 -0500

    added d3 for graph visualizations

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import * as d3 from "d3";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>hello</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
