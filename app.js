698689f9c0966c2855b11d172269c7de9b6f5401
Chris Pfaff <chrispfaff10@gmail.com>
2019-09-27 16:27:17 -0500

    added file input

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 8080;
 const path = require("path");
 
+let corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("/getFile", (req, res) => {
+  console.log("hello here");
+  res.end();
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
