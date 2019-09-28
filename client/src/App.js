5cd9a4bff190e1ca40dfef6f0516d48f41efb563
Chris Pfaff <chrispfaff10@gmail.com>
2019-09-28 16:48:13 -0500

    numbers are readt

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import Graph from "./components/Graph.js";
 import axios from "axios";
 import "./App.css";
+import { isNumber } from "util";
 
 class App extends Component {
   constructor(props) {
     super();
     this.state = {
       file: null,
-      fileLoaded: false
+      fileLoaded: false,
+      fileData: []
     };
 
     this.fileInput = React.createRef();
@@ -22,7 +24,20 @@ class App extends Component {
     data.append("file", this.state.file, this.state.file.name);
 
     axios.post("/getFile", data).then(res => {
-      console.log("success");
+      const populations = res.data
+        .split(" ")
+        .map(item => {
+          if (Number(item) !== 0 && !isNaN(Number(item))) {
+            return Number(item.slice(0, 1));
+          }
+        })
+        .filter(item => {
+          if (isNumber(item)) {
+            return item;
+          }
+        });
+      this.setState({ fileData: populations });
+      console.log(this.state.fileData);
     });
   }
 
