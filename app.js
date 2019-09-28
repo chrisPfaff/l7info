ee053edd87c8b440312faf0e05228adcd595e7b9
Chris Pfaff <chrispfaff10@gmail.com>
2019-09-28 16:48:00 -0500

    numbers are readt

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs");
 const fileUpload = require("express-fileupload");
 
 const port = 8080;
@@ -11,17 +12,28 @@ let corsOptions = {
   optionsSuccessStatus: 200
 };
 
+let fileBuffer;
+
 app.use(fileUpload());
 app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
+const run = () => {
+  console.log(fileBuffer);
+};
+
 app.post("/getFile", (req, res, next) => {
-  console.log(req.files);
-  let uploadFile = req.files.file;
-  const fileName = req.files.file.name;
-  console.log(uploadFile);
-  uploadFile.mv(`${__dirname}/client/public/uploads/${fileName}`);
-  res.end();
+  const uploadFile = req.files.file;
+  res.send(uploadFile.data.toString().replace(/(\r\n|\n|\r|\t)/gm, " "));
+  // uploadFile.mv(`${__dirname}/client/public/uploads/${fileName}`);
+  // fs.readFile(uploadFile, (error, data) => {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     fileBuffer = data;
+  //   }
+  // });
+  // run();
 });
 
 app.get("/", (req, res) => {
