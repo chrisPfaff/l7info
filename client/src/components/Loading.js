5428f9451ebcdd6d4fc19ffcc6d319937b2f2fe7
Chris Pfaff <chrispfaff10@gmail.com>
2019-09-29 15:58:17 -0500

    added loading animation

---
+++ client/src/components/Loading.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../App.css";
+export default class Loading extends Component {
+  render() {
+    return (
+      <div className="loading">
+        <img
+          className="loading_img"
+          src="https://www.l7informatics.com/wp-content/uploads/2018/05/icon1.png"
+          alt="loading image"
+        />
+      </div>
+    );
+  }
+}
