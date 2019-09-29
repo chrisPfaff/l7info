85404c8f8ac1affa6aef568ce9f0eabe2323a7cf
Chris Pfaff <chrispfaff10@gmail.com>
2019-09-29 16:32:25 -0500

    styled graph

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Chart from "chart.js";
-import Loading from "./Loading.js";
 import "../App.css";
 
 export default class Graph extends Component {
@@ -20,24 +19,29 @@ export default class Graph extends Component {
         labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
         datasets: [
           {
-            label: "Percentage",
+            label: "Population Percentage",
             data: [12, 19, 3, 5, 2, 3, 4, 34, 4],
             backgroundColor: [
-              // "rgba(54, 162, 235, 0.2)",
-              // "rgba(255, 99, 132, 0.2)",
-              // "rgba(54, 162, 235, 0.2)",
-              // "rgba(255, 206, 86, 0.2)",
-              // "rgba(75, 192, 192, 0.2)",
-              // "rgba(153, 102, 255, 0.2)",
-              // "rgba(255, 159, 64, 0.2)"
+              "#D53963",
+              "#D53963",
+              "#D53963",
+              "#D53963",
+              "#D53963",
+              "#D53963",
+              "#D53963",
+              "#D53963",
+              "#D53963"
             ],
             borderColor: [
-              // "rgba(255,99,132,1)",
-              // "rgba(54, 162, 235, 1)",
-              // "rgba(255, 206, 86, 1)",
-              // "rgba(75, 192, 192, 1)",
-              // "rgba(153, 102, 255, 1)",
-              // "rgba(255, 159, 64, 1)"
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77"
             ],
             borderWidth: 1
           },
@@ -45,20 +49,26 @@ export default class Graph extends Component {
             label: "Benfords Percentage",
             data: [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6],
             backgroundColor: [
-              // "rgba(255, 99, 132, 0.2)",
-              // "rgba(54, 162, 235, 0.2)",
-              // "rgba(255, 206, 86, 0.2)",
-              // "rgba(75, 192, 192, 0.2)",
-              // "rgba(153, 102, 255, 0.2)",
-              // "rgba(255, 159, 64, 0.2)"
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77",
+              "#422C77"
             ],
             borderColor: [
-              // "rgba(255,99,132,1)",
-              // "rgba(54, 162, 235, 1)",
-              // "rgba(255, 206, 86, 1)",
-              // "rgba(75, 192, 192, 1)",
-              // "rgba(153, 102, 255, 1)",
-              // "rgba(255, 159, 64, 1)"
+              "#AA2F7B",
+              "#AA2F7B",
+              "#AA2F7B",
+              "#AA2F7B",
+              "#AA2F7B",
+              "#AA2F7B",
+              "#AA2F7B",
+              "#AA2F7B",
+              "#AA2F7B"
             ],
             borderWidth: 1
           }
